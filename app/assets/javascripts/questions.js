5563b5133ad9f3e9332191c666f703e39c53530a
Karolinka <li-nee@gmx.de>
2014-11-03 16:45:08 +0100

    visualised userdata roughly, need to make turbolinks work

---
+++ app/assets/javascripts/questions.js
@@ -0,0 +1,50 @@
+var ready;
+
+
+ready = function(){
+
+	d3.json("/kids.json", function(error, json) {
+
+	  data = json
+	  
+	  var svg = d3.select("body").select("#mainkids")
+	                             .attr("height", 600)
+			                				 .attr("width", 1000)
+
+		var xscale = d3.scale.linear().domain([d3.min(data), d3.max(data)]).range([0,width]);
+		var yscale = d3.scale.linear().domain([d3.min(data), d3.max(data)]).range([0,height]);
+		
+			                				 
+
+	  svg.selectAll("circle").data(data)
+			                .enter()
+			                .append("circle")
+			                .attr("r", 0)
+			                .attr("cy", function(d, i) {
+			                    return 20*d.hours
+			                })
+			                .attr("cx", function(d, i) {
+			                    return 50 * i
+			                })
+			                .style('fill', 'deeppink')
+			                .attr("height", 10)
+			                .attr("width", 20)
+			                .attr('transform', "translate(50 ,50 )")
+			                .transition()
+			                .duration(1000) 
+			                .delay(100)
+			                .attr("r",5); 
+
+
+
+		})
+
+}
+
+$(document).ready(ready);
+
+
+
+
+
+
